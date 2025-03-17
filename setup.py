456da04a12fc35344eb6772445ec20a7bdf59281
Wenbin Fang <wenbin@listennotes.com>
2025-03-17 11:56:04 -0700

    Support newer python versions.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'requests >= 2.20; python_version >= "3.0"',
         "setuptools>=41.0.1",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.10",
     project_urls={
         "Bug Tracker": (
             "https://github.com/listennotes/" "podcast-api-python/issues"
@@ -49,10 +49,10 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
